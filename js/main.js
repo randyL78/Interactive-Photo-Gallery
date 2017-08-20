fa7fef84ce5b1288b0b36478bc6f07251c4a9637
Randy Layne <randydavidL78@gmail.com>
2017-08-20 18:01:26 -0400

    fixed a couple js validation errors

--- js/main.js
+++ js/main.js
@@ -11,26 +11,26 @@ var searchInput = document.getElementById("search");
 function searchList() {
    "use strict";
    var searchFilter = searchInput.value.toLowerCase();
-   var searchList = document.getElementById("searchList");
-   var searchItem = searchList.getElementsByTagName('li');
+   var searchUL = document.getElementById("searchList");
+   var searchItem = searchUL.getElementsByTagName("li");
    
    // Loop through entire gallery, hide elements that don't match search terms
    for (var i = 0; i < searchItem.length; i++) {
-      var searchImg = searchItem[i].getElementsByTagName('img')[0];
-      var title = searchImg.getAttribute('title');
-      var alt = searchImg.getAttribute('alt');
+      var searchImg = searchItem[i].getElementsByTagName("img")[0];
+      var title = searchImg.getAttribute("title");
+      var alt = searchImg.getAttribute("alt");
       
       // check if search filter appears in image title or alt attributes
       if (title.toLowerCase().indexOf(searchFilter) > -1 || alt.toLowerCase().indexOf(searchFilter) > -1) {
-         searchItem[i].style.display = "";    
+         searchItem[i].style.display = ""; 
       } else {
-         searchItem[i].style.display = "none"; 
+         searchItem[i].style.display = "none";
       }
    }
 }
 
 // Event handlers
-searchInput.addEventListener('keyup', searchList, false);
+searchInput.addEventListener("keyup", searchList, false);
 
 
 
@@ -38,4 +38,4 @@ searchInput.addEventListener('keyup', searchList, false);
 // Activate Lightcase Plugin
 /////////////////////////////////////////
 
-$('a[data-rel^=lightcase]').lightcase();
+$("a[data-rel^=lightcase]").lightcase();
